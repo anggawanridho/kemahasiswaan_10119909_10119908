e3740c10f054ea12cb9586731a77cc758ade4526
anggawanridho <anggawanridho@gmail.com>
2021-08-07 21:51:21 +0700

    Initial commit

---
+++ src/kemahasiswaan_10119909_10119908/Kemahasiswaan_10119909_10119908.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kemahasiswaan_10119909_10119908;
+
+/**
+ *
+ * @author anggawanridho
+ */
+public class Kemahasiswaan_10119909_10119908 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_utama utama = new frm_utama();
+        utama.setVisible(true);
+    }
+    
+}
